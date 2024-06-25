bf7d27f124d1a6e83060abc74c050b4a39e428c2
ErikKleer <134462187+ErikKleer@users.noreply.github.com>
2024-06-25 12:47:20 -0300

    Criado validação do botão 'Entrar'

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button<{ disabled?: boolean }>`
+    width: 100%;
+    height: 42px;
+    background-color: #81259D;
+    color: #FFF;
+
+    border: 1px solid #81259D;
+    border-radius: 21px;
+
+    &:disabled {
+        pointer-events: none;
+        opacity: 0.6;
+    }
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
